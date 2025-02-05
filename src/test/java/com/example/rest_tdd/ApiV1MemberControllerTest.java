51530bd08f05c33278c567d1fe1f6b1a476b6f81
kozae00 <rhwogus777@gmail.com>
2025-02-05 09:31:12 +0900

    login apiKey add

--- src/test/java/com/example/rest_tdd/ApiV1MemberControllerTest.java
+++ src/test/java/com/example/rest_tdd/ApiV1MemberControllerTest.java
@@ -99,30 +99,44 @@ class ApiV1MemberControllerTest {
     @Test
     @DisplayName("로그인")
     void login() throws Exception {
+
+        String username = "user1";
+        String password = "user11234";
+
+        // 요청
         ResultActions resultActions = mvc
                 .perform(
                         post("/api/v1/members/login")
                                 .content("""
                                         {
-                                            "username": "user1",
-                                            "password": "1234",
+                                            "username": "%s",
+                                            "password": "%s"
                                         }
-                                        """.stripIndent())
+                                        """
+                                        .formatted(username, password)
+                                        .stripIndent())
                                 .contentType(
                                         new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
                                 )
                 )
                 .andDo(print());
+
+        Member member = memberService.findByUsername(username).get();
+
+        // 응답. (요청 처리 결과)
         resultActions
                 .andExpect(status().isOk())
                 .andExpect(handler().handlerType(ApiV1MemberController.class))
                 .andExpect(handler().methodName("login"))
                 .andExpect(jsonPath("$.code").value("200-1"))
-                .andExpect(jsonPath("$.msg").value("$s님 환영합니다.".formatted("유저1")))
+                .andExpect(jsonPath("$.msg").value("%s님 환영합니다.".formatted(member.getNickname())))
                 .andExpect(jsonPath("$.data").exists())
-                .andExpect(jsonPath("$.data.id").isNumber())
-                .andExpect(jsonPath("$.data.nickname").value("유저1"))
-                .andExpect(jsonPath("$.data.createdDate").exists())
-                .andExpect(jsonPath("$.data.modifiedDate").exists());
+                .andExpect(jsonPath("$.data.item.id").isNumber())
+                .andExpect(jsonPath("$.data.item.nickname").value(member.getNickname()))
+                .andExpect(jsonPath("$.data.item.createdDate").exists())
+                .andExpect(jsonPath("$.data.item.modifiedDate").exists())
+                .andExpect(jsonPath("$.data.apiKey").exists());
+
+
     }
 }
